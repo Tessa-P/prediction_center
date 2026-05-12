494a7ffe208661a65a6da5eb40caf94f39acf48c
Tessa-P <97847763+Tessa-P@users.noreply.github.com>
2026-05-11 20:10:52 -0400

    outputs dataframes in desired format

--- scripts/import/UN_population.py
+++ scripts/import/UN_population.py
@@ -1,85 +1,64 @@
-from importlib.metadata import metadata
+from pandas.api.typing.aliases import Axis
 import pandas as pd
 from utils import get_UN_headers, read_un_sheet
 
 FILE = "../WPP2000/WPP2000_EXCEL_FILES/DB02_Stock_Indicators/WPP2000_DB2_F1_TOTAL_POPULATION_BOTH_SEXES.xls"
-# df = pd.read_excel(FILE, sheet_name="MEDIUM", header=None, nrows=18)
 
 
-def get_single_sheet():
-    sheet = "MEDIUM"
-    df = read_un_sheet(FILE, sheet)
+def get_dataframe_from_file(file: str) -> pd.DataFrame:
+    """
+    Pulls all relevant data from a given file.  Takes a file path as input and outputs a combined and labelled dataframe of only the relevant data
+    """
 
-    df = df.rename(columns = {
-        'Major area, region, country or area': 'region_name',
-        'Country code': 'iso_num'
-    })
+    # stitching all sheets together
 
-    year_columns = [col for col in df.columns if str(col).isdigit()]
-    metadata_columns = [col for col in df.columns if not str(col).isdigit()]
-    print(year_columns)
-    print(metadata_columns)
+    # pop off notes sheet
+    all_sheets = pd.ExcelFile(file).sheet_names
+    sheets_to_read = all_sheets[:-1]
 
-    df = df.melt(
-        id_vars=metadata_columns, #columns to keep
-        value_vars=year_columns, #columns to pivot
-        var_name="for_year", #new name of header column
-        value_name="value" #new name of value column
-    )
+    dfs = []
+    for sheet in sheets_to_read:
+        df = read_un_sheet(FILE, sheet)
 
-    print(df.dtypes)
+        # rename columns
+        df = df.rename(
+            columns={
+                "Major area, region, country or area": "region_name",
+                "Country code": "iso_num",
+            }
+        )
 
-    # want to cast for_year to int
-    df["for_year"] = df["for_year"].astype(int)
+        # parse year vs metadata headers
+        year_columns = [col for col in df.columns if str(col).isdigit()]
+        metadata_columns = [col for col in df.columns if not str(col).isdigit()]
 
-    # want to multiply value by 1000, then cast to int
-    df["value"] = (df["value"]*1000).astype(int)
+        # make tall
+        df = df.melt(
+            id_vars=metadata_columns,  # columns to keep
+            value_vars=year_columns,  # columns to pivot
+            var_name="for_year",  # new name of header column
+            value_name="value",  # new name of value column
+        )
 
-    # assigning more metadata
-    df["gender"] = "Both"
-    df["scenario"] = sheet
+        # cast to new types
+        df["for_year"] = df["for_year"].astype(int)
+        df["value"] = (df["value"] * 1000).astype(int)
 
-    print(df.head(10))
-    print(df.dtypes)
+        # attach metadata
+        df["gender"] = "Both"
+        df["scenario"] = sheet
 
-# stitching all sheets together
+        dfs.append(df)
 
-# 1 - pop off notes sheet
-all_sheets = pd.ExcelFile(FILE).sheet_names
-sheets_to_read = all_sheets[:-1]
+    dfs_combined = pd.concat(dfs, ignore_index=True)
 
-dfs = []
-for sheet in sheets_to_read:
-    df = read_un_sheet(FILE, sheet)
-    
-    # rename columns
-    df = df.rename(columns = {
-        'Major area, region, country or area': 'region_name',
-        'Country code': 'iso_num'
-    })
+    # delete notes I don't care about
+    dfs_combined = dfs_combined.drop(["Index", "Notes"], axis=1)
 
-    # parse year vs metadata headers
-    year_columns = [col for col in df.columns if str(col).isdigit()]
-    metadata_columns = [col for col in df.columns if not str(col).isdigit()]
+    return dfs_combined
 
-    # make tall
-    df = df.melt(
-        id_vars=metadata_columns, #columns to keep
-        value_vars=year_columns, #columns to pivot
-        var_name="for_year", #new name of header column
-        value_name="value" #new name of value column
-    )
 
-    # cast to new types
-    df["for_year"] = df["for_year"].astype(int)
-    df["value"] = (df["value"]*1000).astype(int)
+full_data = get_dataframe_from_file(FILE)
+print(full_data)
 
-    # attach metadata
-    df["gender"] = "Both"
-    df["scenario"] = sheet
-
-    dfs.append(df)
-
-dfs_combined = pd.concat(dfs, ignore_index=True)
-print(f"Combined Shape: {dfs_combined.shape}")
-print(dfs_combined["gender"].unique())
+# take data and push it to database
