54036c7c9dc2e13a6a05227fcb23351e69b30d2f
Tessa-P <97847763+Tessa-P@users.noreply.github.com>
2026-05-13 18:51:46 -0400

    Populates WPP2000 data

--- scripts/import/UN_population.py
+++ scripts/import/UN_population.py
@@ -1,8 +1,19 @@
+from pathlib import Path
 import pandas as pd
+import re
 
 from sqlmodel import Session, select
 from app.core.db import engine
-from app.models import Datatype, Age, Gender, Region
+from app.models import (
+    Datatype,
+    Age,
+    Gender,
+    Region,
+    Context,
+    Datapoint,
+    Scenario,
+    Report,
+)
 
 from utils import get_UN_headers, read_un_sheet
 
@@ -19,7 +30,8 @@ def get_dataframe_from_file(file: str) -> pd.DataFrame:
 
     # pop off notes sheet
     all_sheets = pd.ExcelFile(file).sheet_names
-    sheets_to_read = all_sheets[:-1]
+    sheets_to_read = all_sheets[:4]  # only constructs estimates, low, high, medium
+    # TODO: other sheets may have different sheets where you don't just want the first 4
 
     dfs = []
     for sheet in sheets_to_read:
@@ -41,19 +53,20 @@ def get_dataframe_from_file(file: str) -> pd.DataFrame:
         df = df.melt(
             id_vars=metadata_columns,  # columns to keep
             value_vars=year_columns,  # columns to pivot
-            var_name="for_year",  # new name of header column
+            var_name="year_analyzed",  # new name of header column
             value_name="value",  # new name of value column
         )
 
         # cast to new types
-        df["for_year"] = df["for_year"].astype(int)
+        df["year_analyzed"] = df["year_analyzed"].astype(int)
         df["value"] = (df["value"] * 1000).astype(int)
 
         # attach metadata
-        df["datatype"] = "Population"
-        df["age_group"] = "None"
-        df["gender"] = "Both"
+        df["datatype"] = "Population"  # TODO: pull from sheet properly
+        df["age_group"] = "None"  # TODO: pull from sheet properly
+        df["gender"] = "Both"  # TODO: pull from sheet properly
         df["scenario"] = sheet
+        # TODO: this data exists in the "variant" col, can pull from that directly (and dont have to deal with the regex nonsense)
 
         dfs.append(df)
 
@@ -69,55 +82,143 @@ def get_dataframe_from_file(file: str) -> pd.DataFrame:
     return dfs_combined
 
 
-def get_series_ids(series: pd.Series):
+def get_series_ids(
+    series: pd.Series, datatype_lookup, age_lookup, gender_lookup, region_lookup
+) -> dict[str, int]:
     """
-    takes as input a single dataseries, outputs the relevant FKs to the datatype, age, gender, and region tables (in that order)
+    takes as input a single dataseries, returns a Context object with the correct IDs
     """
-    # generate series info
-    # determine what the series should be across datatype (ex. population), age group, gender, region
-    # datatype: column "datatype"
-    # age: column "age_group"
-    # gender: column "gender"
-    # region: column "iso_num"
 
     metadata = series.loc[["datatype", "age_group", "gender", "iso_num"]]
 
+    datatype_id = datatype_lookup.get(metadata["datatype"])
+    if datatype_id is None:
+        raise ValueError(f"datatype '{metadata["datatype"]}' not found")
+
+    age_id = age_lookup.get(metadata["age_group"])
+    if age_id is None:
+        raise ValueError(f"age group '{metadata["age_group"]}' not found")
+
+    gender_id = gender_lookup.get(metadata["gender"])
+    if gender_id is None:
+        raise ValueError(f"gender '{metadata["gender"]}' not found")
+
+    # requires having all regions pre-existing in the regions table
+    region_id = region_lookup.get(metadata["iso_num"])
+    if region_id is None:
+        raise ValueError(
+            f"region num '{metadata["iso_num"]}' ('{series.loc["region_name"]}') not found"
+        )
+
+    new_context = {
+        "datatype_id": datatype_id,
+        "age_id": age_id,
+        "gender_id": gender_id,
+        "region_id": region_id,
+    }
+    return new_context
+
+
+def build_key(context_dict: dict[str, int]) -> tuple[int, int, int, int]:
+    """
+    takes in a dictionary of header : id pairs, builds a tuple of the form (datatype_id, age_id, gender_id, region_id)
+    """
+    return (
+        context_dict["datatype_id"],
+        context_dict["age_id"],
+        context_dict["gender_id"],
+        context_dict["region_id"],
+    )
+
+
+def get_scenario(scenario: str, scenario_lookup) -> int:
+    scenario_id = None
+    for scenario_description in scenario_lookup:
+        if re.search(scenario_description, scenario, re.IGNORECASE):
+            scenario_id = scenario_lookup.get(scenario_description)
+            break
+    if scenario_id is None:
+        raise ValueError(f"scenario '{scenario}' not found")
+    return scenario_id
+
+
+def get_report_id(file: str) -> int:
+    """
+    input: a string representing the file location.
+    output: the corresponding id in the database
+    """
+    path = Path(file)
+    year_published = int(path.stem[3:7])
     with Session(engine) as session:
-        datatype_table = {
+        # TODO: assumes only one report per year, should match on other characteristics to guarantee uniqueness
+        report = session.exec(select(Report).where(Report.year_published == year_published)).first()
+        if report is None:
+            raise ValueError(f"report for published year '{year_published}' not found")
+    if report.id is None:
+        raise ValueError(f"report PK is 'None'")
+    return report.id
+
+
+def populate_datapoints(df: pd.DataFrame, report_id: int):
+    """
+    takes as input a dataframe, populates the datapoint table with all data in the dataframe
+    """
+    with Session(engine) as session:
+        datatype_lookup = {
             d.datatype_description: d.id for d in session.exec(select(Datatype)).all()
         }
-        datatype_id = datatype_table.get(metadata["datatype"])
-        if datatype_id is None:
-            raise ValueError(f"datatype '{metadata["datatype"]}' not found")
-
-        age_table = {a.age_group: a.id for a in session.exec(select(Age)).all()}
-        age_id = age_table.get(metadata["age_group"])
-        if age_id is None:
-            raise ValueError(f"age group '{metadata["age_group"]}' not found")
-
-        gender_table = {
+        age_lookup = {a.age_group: a.id for a in session.exec(select(Age)).all()}
+        gender_lookup = {
             g.gender_group: g.id for g in session.exec(select(Gender)).all()
         }
-        gender_id = gender_table.get(metadata["gender"])
-        if gender_id is None:
-            raise ValueError(f"gender '{metadata["gender"]}' not found")
+        region_lookup = {r.iso_num: r.id for r in session.exec(select(Region)).all()}
+        # builds key directly from the context object
+        context_lookup = {
+            (c.datatype_id, c.age_id, c.gender_id, c.region_id): c.id
+            for c in session.exec(select(Context)).all()
+        }
+        scenario_lookup = {
+            s.scenario_description: s.id for s in session.exec(select(Scenario)).all()
+        }
+        for _, row in df.iterrows():
+            context_dict = get_series_ids(
+                row, datatype_lookup, age_lookup, gender_lookup, region_lookup
+            )
+            key = build_key(context_dict)
+            context_id = context_lookup.get(key)
+            if context_id is None:  # if there is no matching context
+                # push new context to the Context table
+                context = Context(
+                    datatype_id=context_dict["datatype_id"],
+                    age_id=context_dict["age_id"],
+                    gender_id=context_dict["gender_id"],
+                    region_id=context_dict["region_id"],
+                )
+                session.add(context)
+                session.flush()
+                session.refresh(context)
+                context_id = context.id
+                context_lookup[key] = context_id
+                if context_id is None:  # check in testing
+                    raise ValueError(f"context not populated - key: {key}")
 
-        # requires having all regions pre-loaded
-        region_table = {r.iso_num: r.id for r in session.exec(select(Region)).all()}
-        region_id = region_table.get(metadata["iso_num"])
-        if region_id is None:
-            raise ValueError(
-                f"region num '{metadata["iso_num"]}' ('{series.loc["region_name"]}') not found"
+            # context object is now in database, context_id holds the relevant id (also accessible with context.id)
+            new_datapoint = Datapoint(
+                report_id=report_id,  # TODO: Can handle UN reports, can't do anything else
+                context_id=context_id,
+                scenario_id=get_scenario(row["scenario"], scenario_lookup),
+                year_analyzed=row["year_analyzed"],
+                value=row["value"],
             )
 
-        return [datatype_id, age_id, gender_id, region_id]
+            session.add(new_datapoint)
+        session.commit()
 
 
 full_data = get_dataframe_from_file(FILE)
-# print(full_data)
-for _, row in full_data.iterrows():
-    ids = get_series_ids(row)
-    
-
+print(full_data)
+report_id = get_report_id(FILE)
+populate_datapoints(full_data, report_id)
+print("success")
 
 # take data and push it to database
